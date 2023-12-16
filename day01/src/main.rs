7e7297691a3fa0a900c98a56eb186ecf3e47be26
garvinob <garv@garvbox.net>
2023-12-16 16:43:44 +0000

    Switch input to borrowed ref
    Allows use of input String data for both parts
    of the challenge

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,11 +2,13 @@ use std::fs;
 
 pub fn main() {
     let file_content = fs::read_to_string("day01/data/input.txt").unwrap();
-    let result = part01(file_content);
-    println!("Part 1 Result {}", result)
+    let result_part1 = part01(&file_content);
+    println!("Part 1 Result {}", result_part1);
+    let result_part2 = part02(&file_content);
+    println!("Part 2 Result {}", result_part2)
 }
 
-fn part01(input: String) -> String {
+fn part01(input: &String) -> String {
     input
         .lines()
         .map(|line| {
@@ -24,7 +26,7 @@ fn part01(input: String) -> String {
         .to_string()
 }
 
-fn part02(input: String) -> String {
+fn part02(input: &String) -> String {
     281.to_string()
 }
 
@@ -39,7 +41,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"
             .to_string();
-        assert_eq!("142", part01(test_input));
+        assert_eq!("142", part01(&test_input));
     }
 
     #[test]
@@ -52,6 +54,6 @@ xtwone3four
 zoneight234
 7pqrstsixteen"
             .to_string();
-        assert_eq!("281", part02(test_input));
+        assert_eq!("281", part02(&test_input));
     }
 }
