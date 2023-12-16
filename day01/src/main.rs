872e69d589d12c477a37632f544fc396507fe005
garvinob <garv@garvbox.net>
2023-12-16 16:11:12 +0000

    Add solution day01

---
+++ day01/src/main.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+pub fn main() {
+    let file_content = fs::read_to_string("day01/data/input.txt").unwrap();
+    let result = part01(file_content);
+    println!("Part 1 Result {}", result)
+}
+
+fn part01(input: String) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|character| character.to_digit(10));
+            let first = digits
+                .next()
+                .expect(format!("Could not find any digit in line {}", line).as_str());
+            let last = match digits.last() {
+                Some(v) => v,
+                None => first,
+            };
+            first * 10 + last
+        })
+        .sum::<u32>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let test_input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+            .to_string();
+        assert_eq!("142", part01(test_input));
+    }
+}
