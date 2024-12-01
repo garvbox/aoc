efb41a418fcee59c6678ab1186541714aa7cafac
garvbox <garv@garvbox.net>
2024-12-01 21:03:52 +0000

    Day 1 part 2 complete

--- 2024/day-01/src/part2.rs
+++ 2024/day-01/src/part2.rs
@@ -1,17 +1,43 @@
+use std::collections::HashMap;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day_01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut lefts: Vec<usize> = vec![];
+    let mut rights: Vec<usize> = vec![];
+
+    for line in input.lines() {
+        let mut split_line = line.split_whitespace();
+        lefts.push(split_line.next().unwrap().parse().unwrap());
+        rights.push(split_line.next().unwrap().parse().unwrap());
+    }
+
+    let mut counts: HashMap<usize, usize> = HashMap::new();
+    let mut total: usize = 0;
+
+    for l in lefts {
+        let num_occurences = counts
+            .entry(l)
+            .or_insert_with(|| rights.iter().filter(|x| **x == l).count());
+        total += *num_occurences * l;
+        tracing::trace!("Got num_occurences: {} for entry {}", num_occurences, l);
+    }
+
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
