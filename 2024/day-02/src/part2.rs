c74a8c9001cabaa3cb74b1819d5176b81b09474c
garvinobrien <garvin.obrien@keelvar.com>
2024-12-03 11:31:47 +0000

    Solve day-02 part2

--- 2024/day-02/src/part2.rs
+++ 2024/day-02/src/part2.rs
@@ -1,17 +1,53 @@
-#[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day_02 - part 2");
+use crate::part1;
+
+// #[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let n: usize = input
+        .lines()
+        .map(|line| {
+            tracing::debug!("Line: {:?}", line);
+            let nums: Vec<isize> = line
+                .split_whitespace()
+                .into_iter()
+                .map(|n| n.parse().unwrap())
+                .collect();
+
+            if part1::is_line_safe(nums.clone()) {
+                tracing::trace!("Line Safe: {:?}", line);
+                return 1;
+            }
+
+            let mut has_save_variant = 0;
+            for index in 0..nums.len() {
+                let mut removed_one = nums.clone();
+                removed_one.remove(index);
+                tracing::trace!("Testing Line Variant: {:?}", removed_one.clone());
+                if part1::is_line_safe(removed_one) {
+                    tracing::trace!("Safe Line Variant");
+                    has_save_variant += 1;
+                    break; // This is critical as we only want to count one safe variant per line
+                }
+            }
+            has_save_variant
+        })
+        .sum();
+
+    Ok(n.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("4", process(input)?);
         Ok(())
     }
 }
