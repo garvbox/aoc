da258671af540b5afa76c2148035f793e8622604
garvbox <garv@garvbox.net>
2024-12-08 18:00:01 +0000

    Init day-05

---
+++ 2024/day-05/src/part1.rs
@@ -0,0 +1,17 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day_05 - part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_log::test;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "";
+        assert_eq!("", process(input)?);
+        Ok(())
+    }
+}
