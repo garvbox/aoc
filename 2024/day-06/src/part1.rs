c83c329ca04ad04b1a7eb1d4c881ed0945d3b14e
garvinobrien <garvin.obrien@keelvar.com>
2024-12-23 17:50:58 +0000

    Add structs for modelling entities

--- 2024/day-06/src/part1.rs
+++ 2024/day-06/src/part1.rs
@@ -1,6 +1,36 @@
+use glam::UVec2;
+
 #[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> miette::Result<String> {
 #[tracing::instrument]
+#[derive(Eq, PartialEq, Clone, Debug)]
+enum Entity {
+    Obstruction(UVec2),
+    Guard(GuardPosition),
+}
+
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
+struct GuardPosition {
+    position: UVec2,
+    direction: GuardDirection,
+}
+
+impl GuardPosition {
+    fn new(x: u32, y: u32) -> GuardPosition {
+        GuardPosition {
+            position: UVec2::from((x, y)),
+            direction: GuardDirection::North,
+        }
+    }
+}
+
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
+enum GuardDirection {
+    #[default]
+    North,
+    South,
+    East,
+    West,
 }
 
 #[cfg(test)]
