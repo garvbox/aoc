beb1089fe07573b57080493a444cb81f9f7f27fb
garvbox <garv@garvbox.net>
2024-12-23 13:34:56 +0000

    Setup day-06

---
+++ 2024/day-06/src/part2.rs
@@ -0,0 +1,17 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day_06 - part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_log::test;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "";
+        assert_eq!("", process(input)?);
+        Ok(())
+    }
+}
