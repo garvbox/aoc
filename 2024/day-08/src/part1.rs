4e986b8f8fdb6cad56b4fcf886ecb8842b020fc2
garvbox <garv@garvbox.net>
2025-01-01 11:39:34 +0000

    Add bounds checks
    Switched to ivec so that we can handle antinodes below zero point and
    check them in bounds

--- 2024/day-08/src/part1.rs
+++ 2024/day-08/src/part1.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use glam::UVec2;
+use glam::IVec2;
 
 #[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> miette::Result<String> {
@@ -19,16 +19,29 @@ pub fn process(input: &str) -> miette::Result<String> {
         }
     }
 
+    let max_x = input
+        .lines()
+        .skip_while(|line| line.is_empty())
+        .next()
+        .unwrap()
+        .len() as i32;
+    let max_y = input.lines().skip_while(|line| line.is_empty()).count() as i32;
+    let bounds = IVec2::new(max_x, max_y);
+
     // TODO: Collect all possible pairs of antennae (iterools.combinations?) and work out antinodes
     // for each in a HashSet
 
     Ok("".to_owned())
 }
 
+fn in_bounds(position: &IVec2, bound: &IVec2) -> bool {
+    position.cmple(*bound).all() && position.cmpge(IVec2::new(0, 0)).all()
+}
+
 #[derive(Eq, PartialEq, Clone, Debug)]
 struct Antenna {
     character: char,
-    position: UVec2,
+    position: IVec2,
 }
 
 #[tracing::instrument]
@@ -37,7 +50,7 @@ fn parse_row(row: usize, input: &str) -> Vec<Antenna> {
         .chars()
         .enumerate()
         .filter_map(|(column, ch)| {
-            let pos = UVec2::from((column as u32, row as u32));
+            let pos = IVec2::from((column as i32, row as i32));
             let res = match ch {
                 '.' => None,
                 _other => {
@@ -75,6 +88,30 @@ mod tests {
 ............
 ";
         assert_eq!("14", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_in_bounds_true() -> miette::Result<()> {
+        let bounds = IVec2::new(2, 2);
+        for position in [(0, 0), (0, 1), (2, 2)] {
+            tracing::trace!("Checking position: {:?} in bounds {:?}", position, bounds);
+            assert!(in_bounds(&IVec2::new(position.0, position.1), &bounds));
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_in_bounds_false() -> miette::Result<()> {
+        let bounds = IVec2::new(2, 2);
+        for position in [(-1, 0), (0, -1), (3, 2), (2, 3), (3, 3)] {
+            tracing::trace!(
+                "Checking position: {:?} not in bounds {:?}",
+                position,
+                bounds
+            );
+            assert!(!in_bounds(&IVec2::new(position.0, position.1), &bounds));
+        }
         Ok(())
     }
 }
