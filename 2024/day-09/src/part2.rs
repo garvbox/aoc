d3aa8745caf90d869a7d5f2e1cbd61d82963ba45
garvbox <garv@garvbox.net>
2025-01-02 16:52:05 +0000

    Implement adjusted input reader for part2
    Storing files and gaps as a struct to make it easier to moved them
    around as a whole

--- 2024/day-09/src/part2.rs
+++ 2024/day-09/src/part2.rs
@@ -1,7 +1,56 @@
+use itertools::Itertools;
+use miette::miette;
+
 #[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> miette::Result<String> {
-    let _ = input.lines();
-    todo!("day_09 - part 2");
+    let input_line = input.chars().filter(|ch| *ch != '\n');
+    let mut files: Vec<File> = vec![];
+
+    for (file_index, mut chunk) in (&input_line.chunks(2)).into_iter().enumerate() {
+        let file_size: u32 = chunk
+            .next()
+            .ok_or(miette!("Invalid Input chunk"))?
+            .to_digit(10)
+            .ok_or(miette!("Invalid file size"))?;
+
+        let free_space: u32 = chunk
+            .next()
+            .unwrap_or('0') // Last file in a sequence may or may not have free space after
+            .to_digit(10)
+            .ok_or(miette!("Invalid free space"))?;
+
+        tracing::trace!(
+            "File index: {file_index}, File size: {file_size}, Free space: {free_space}"
+        );
+        files.push(File {
+            index: Some(file_index as u32),
+            size: file_size,
+        });
+        files.push(File {
+            index: None,
+            size: free_space,
+        });
+    }
+
+    // Construct formatted output for printing
+    let disk_layout: String = files
+        .iter()
+        .map(|file| {
+            let char = match file.index {
+                Some(index) => index.to_string(),
+                None => ".".to_string(),
+            };
+            char.repeat(file.size as usize)
+        })
+        .collect();
+    tracing::info!("Original Disk Map:\n{}", disk_layout);
+
+    Ok("".to_string())
+}
+
+struct File {
+    index: Option<u32>, // None here means a Gap
+    size: u32,
 }
 
 #[cfg(test)]
@@ -11,8 +60,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "2333133121414131402";
+        assert_eq!("2858", process(input)?);
         Ok(())
     }
 }
