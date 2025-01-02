68de97b5b1f8810d51ca4c9181d789ba4b0d4dfa
garvbox <garv@garvbox.net>
2025-01-02 22:02:25 +0000

    Implement part2 defragmenter
    This is really horrible slow implementation but is easy to read

--- 2024/day-09/src/part2.rs
+++ 2024/day-09/src/part2.rs
@@ -35,10 +35,105 @@ pub fn process(input: &str) -> miette::Result<String> {
         });
         file_position += free_space as usize;
     }
-    print_file_map(files);
+    print_file_map(&files);
+
+    // TODO: The below loops over files and tries to keep track as we go, but what we should
+    // actually be doing is just picking up the file indexes first and repeatedly re-processing the
+    // same list of objects for movement as it would make it a lot easier to keep track of things
+    // than the nested iterators to find gaps and movement candidates
+
+    let file_ids: Vec<u64> = files.clone().iter().filter_map(|f| f.id).rev().collect();
+
+    for file_id in file_ids {
+        files = defrag_move_file(file_id, files);
+        print_file_map(&files);
+    }
+    print_file_map(&files);
+
+    let checksum: u64 = files
+        .iter()
+        .map(|file| match file.id {
+            Some(id) => (file.position..file.position + file.size as usize)
+                .map(|pos| pos as u64 * id)
+                .sum(),
+            None => 0,
+        })
+        .sum();
+
+    Ok(checksum.to_string())
 }
 
-fn print_file_map(files: Vec<File>) {
+fn defrag_move_file(file_id: u64, files: Vec<File>) -> Vec<File> {
+    // New copies of the file and gap for update
+    let mut file = files
+        .iter()
+        .find(|f| f.id == Some(file_id))
+        .expect("Missing file id")
+        .clone();
+    let gap = files
+        .iter()
+        .find(|f| f.id.is_none() && f.size >= file.size && f.position < file.position);
+
+    let mut gap = match gap {
+        Some(gap) => gap.clone(),
+        None => {
+            tracing::debug!("No gap found for file {:?}", file);
+            return files.clone();
+        }
+    };
+
+    // Moving file to a new location will leave a gap
+    let new_file_gap = File {
+        id: None,
+        size: file.size,
+        position: file.position,
+    };
+
+    // Update the file to new position
+    let previous_position = file.position;
+    file.position = gap.position;
+    tracing::trace!(
+        "Updated file {:?} position from {:?} to {:?}",
+        file_id,
+        previous_position,
+        file.position
+    );
+
+    // Drop gap size and move as needed
+    if gap.size == file.size {
+        gap.size = 0;
+        tracing::trace!("Dropped Gap at position {:?}", gap.position);
+    } else {
+        gap.size -= file.size;
+        gap.position = file.position + file.size as usize;
+        tracing::trace!(
+            "Moved Gap to position {:?}, size to {:?}",
+            gap.position,
+            gap.size
+        );
+    }
+
+    let mut result: Vec<File> = files
+        .iter()
+        .filter_map(|existing_file| {
+            if existing_file.position == file.position || existing_file.id == file.id {
+                None
+            } else {
+                Some(existing_file.clone())
+            }
+        })
+        .collect();
+
+    result.push(file);
+    result.push(new_file_gap);
+    if gap.size > 0 {
+        result.push(gap);
+    }
+
+    result.into_iter().sorted_by_key(|f| f.position).collect()
+}
+
+fn print_file_map(files: &[File]) -> String {
     let disk_map: String = files
         .iter()
         .map(|file| {
@@ -49,10 +144,9 @@ fn print_file_map(files: Vec<File>) {
             char.repeat(file.size as usize)
         })
         .collect();
-    tracing::info!("Original Disk Map:\n{}", disk_layout);
 
-    Ok("".to_string())
-    println!("Disk Map:\n{}", disk_map);
+    tracing::trace!("Disk Map:\t{}", disk_map);
+    disk_map
 }
 
 #[derive(Eq, PartialEq, Clone, Debug)]
