885a414a1f8dbc64db955e9f8f3c11369f57183f
garvbox <garv@garvbox.net>
2025-01-02 16:39:10 +0000

    Implement compactor

--- 2024/day-09/src/part1.rs
+++ 2024/day-09/src/part1.rs
@@ -35,7 +35,51 @@ pub fn process(input: &str) -> Result<String> {
         })
         .collect();
     tracing::info!("Original Disk Map:\n{}", disk_layout);
-    Ok("".to_string())
+
+    let mut compacted_files: Vec<u32> = vec![];
+    let mut index = 0;
+    let mut num_popped = 0;
+    let disk_size = files.iter().filter(|el| el.is_some()).count();
+
+    while index < disk_size {
+        tracing::trace!("Working on index: {:?}, num moved {:?}", index, num_popped);
+        match files[index] {
+            Some(block) => compacted_files.push(block),
+            None => {
+                if let Some(block_to_move) = loop {
+                    let candidate = files.pop().expect("Ran out of file blocks");
+                    num_popped += 1;
+                    tracing::trace!("Getting next block: {:?}", candidate);
+                    match candidate {
+                        Some(block) => {
+                            tracing::trace!("Found move candidate: {:?}", block);
+                            break Some(block);
+                        }
+                        None => {
+                            tracing::trace!("Found no move candidate");
+                        }
+                    }
+                } {
+                    tracing::trace!("Moving Block: {block_to_move}");
+                    compacted_files.push(block_to_move);
+                }
+            }
+        }
+        index += 1;
+    }
+
+    let disk_layout_compacted: String = compacted_files
+        .iter()
+        .map(|el| "|".to_owned() + &el.to_string())
+        .collect();
+    tracing::info!("Compacted Disk Map:\n{}", disk_layout_compacted);
+
+    let checksum: u64 = compacted_files
+        .iter()
+        .enumerate()
+        .map(|(index, file_index)| index as u64 * *file_index as u64)
+        .sum();
+    Ok(checksum.to_string())
 }
 
 #[cfg(test)]
