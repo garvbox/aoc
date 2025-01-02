229e719ca01811df63b7d6d0a85c04f6ae7cf7a7
garvbox <garv@garvbox.net>
2025-01-02 15:19:05 +0000

    Implement disk map reading

--- 2024/day-09/src/part1.rs
+++ 2024/day-09/src/part1.rs
@@ -1,7 +1,33 @@
+use itertools::Itertools;
+use miette::{miette, Result};
+
 #[tracing::instrument(skip(input))]
-pub fn process(input: &str) -> miette::Result<String> {
-    let _ = input.lines();
-    todo!("day_09 - part 1");
+pub fn process(input: &str) -> Result<String> {
+    let mut files: Vec<Option<u32>> = vec![];
+
+    for (file_index, (file_size, free_space)) in input.chars().tuple_windows().enumerate() {
+        let file_size: u32 = file_size
+            .to_digit(10)
+            .ok_or(miette!("Invalid file size, got {:?}", file_size))?;
+        let free_space: u32 = free_space
+            .to_digit(10)
+            .ok_or(miette!("Invalid free space, got {:?}", file_size))?;
+
+        tracing::trace!("File: {file_size}, Space: {free_space}");
+        files.extend([Some(file_index as u32)].repeat(file_size as usize));
+        files.extend([None].repeat(free_space as usize));
+    }
+
+    // Construct formatted output for printing
+    let disk_layout: String = files
+        .iter()
+        .map(|el| match el {
+            Some(item) => "|".to_owned() + &item.to_string(),
+            None => "|.".to_string(),
+        })
+        .collect();
+    tracing::info!("Disk Map:\n{}", disk_layout);
+    Ok("".to_string())
 }
 
 #[cfg(test)]
