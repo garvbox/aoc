401ba42394e79a4e01507082ea07b2bbad2fff20
garvbox <garv@garvbox.net>
2025-01-06 14:19:06 +0000

    Add simple point parsing as 3-d points

--- 2024/day-10/src/part1.rs
+++ 2024/day-10/src/part1.rs
@@ -1,7 +1,28 @@
+use glam::IVec3;
+
 #[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> miette::Result<String> {
-    let _ = input.lines();
-    todo!("day_10 - part 1");
+    let points: Vec<IVec3> = input
+        .lines()
+        .rev()
+        .enumerate()
+        .flat_map(move |(row_index, row)| {
+            row.chars().enumerate().filter_map(move |(col_index, col)| {
+                let height = match col {
+                    '.' => return None,
+                    height => height,
+                };
+                Some(IVec3::new(
+                    col_index as i32,
+                    row_index as i32,
+                    height.to_string().parse().unwrap(),
+                ))
+            })
+        })
+        .collect();
+
+    tracing::trace!("Parsed Points: {:?}", points);
+    Ok("".into())
 }
 
 #[cfg(test)]
